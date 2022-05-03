45a6924c327e17877a0964e7cc2640a17515fe26
Happy <1737575350@qq.com>
2022-05-03 22:31:46 +0800

    junit测试读取图片&断言

--- src/test/ImageTest.java
+++ src/test/ImageTest.java
@@ -1,14 +1,13 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-import javax.imageio.ImageIO;
-
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ImageTest {
 
@@ -16,12 +15,11 @@ class ImageTest {
 	void test() {
 		
 		try {
-			BufferedImage image = ImageIO.read(new File("C:/work/javaprojects/Tank_60/src/images/bulletD.gif"));
+			BufferedImage image = ImageIO.read(new File("src/images/bulletD.gif"));
 			assertNotNull(image);
-			
 			BufferedImage image2 = ImageIO.read(ImageTest.class.getClassLoader().getResourceAsStream("images/bulletD.gif"));
 			//this.getClass()
-			assertNotNull(image2);
+			assertNotNull(image2);//断言：测试条件成立就算通过,反之
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
