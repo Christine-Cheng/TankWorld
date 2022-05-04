19121701b3192ad7cd967e2e5802221531d39d23
Happy <1737575350@qq.com>
2022-05-04 17:18:50 +0800

    new出5个敌方坦克

--- src/com/tankwold/Main.java
+++ src/com/tankwold/Main.java
@@ -1,12 +1,19 @@
 package com.tankwold;
 
+import com.tankwold.enums.Direction;
+
 /**
  * @author Happy
  * @create 2022/4/20-0:55
  **/
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        TankFrame frame=new TankFrame();
+        TankFrame frame = new TankFrame();
+        
+        //初始化敌方坦克
+        for (int i = 0; i < 5; i++) {
+            frame.enemyTankList.add(new Tank(50 + i * 80, 200, Direction.DOWN, frame));
+        }
         while (true) {
             Thread.sleep(50);
             frame.repaint(); //You can use threads to refresh the window
--- src/com/tankwold/TankFrame.java
+++ src/com/tankwold/TankFrame.java
@@ -17,9 +17,10 @@ import java.util.List;
  **/
 public class TankFrame extends Frame {
     //set tank's default direction and starting coordinates
-    Tank myTank = new Tank(200, 200, Direction.DOWN, this);//Encapsulate the attributes of the tank into the tank class
+    Tank myTank = new Tank(200, 400, Direction.UP, this);//Encapsulate the attributes of the tank into the tank class
     //Bullet bullet = new Bullet(300, 300, Direction.DOWN);//单个子弹不够用
     List<Bullet> bulletList = new ArrayList<>();//set bullet's container
+    List<Tank> enemyTankList = new ArrayList<>();
     static final int GAME_WIDTH = 800, GAME_HEIGHT = 600;//the game screen size
     
     public TankFrame() {
@@ -122,6 +123,11 @@ public class TankFrame extends Frame {
                 bulletIterator.remove();//Iterator可以进行同步删除
             }
             bullet.paint(graphics);
+        }
+    
+        //paint enemy tanks
+        for (int i = 0; i < enemyTankList.size(); i++) {
+            enemyTankList.get(i).paint(graphics);
         }
     }
     
