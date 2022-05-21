f63623b50196a1366988e2c30c537623afa706cc
Happy <1737575350@qq.com>
2022-05-21 23:50:22 +0800

    更新:添加和区分好坏坦克

--- src/com/tankwold/ResourceManager.java
+++ src/com/tankwold/ResourceManager.java
@@ -11,7 +11,8 @@ import java.io.IOException;
  * @create 2022/5/3-23:00
  **/
 public class ResourceManager {
-    public static BufferedImage tankL, tankU, tankR, tankD;//tank Left Up Right Down
+    public static BufferedImage goodTankL, goodTankU, goodTankR, goodTankD;//tank Left Up Right Down
+    public static BufferedImage badTankL, badTankU, badTankR, badTankD;//tank Left Up Right Down
     public static BufferedImage bulletL, bulletU, bulletR, bulletD;//bullet Left Up Right Down
     public static BufferedImage[] explodeArr = new BufferedImage[16];
     
@@ -22,23 +23,28 @@ public class ResourceManager {
             //tankU = ImageIO.read(ResourceManager.class.getClassLoader().getResourceAsStream("images/tankU.gif"));
             //tankR = ImageIO.read(ResourceManager.class.getClassLoader().getResourceAsStream("images/tankR.gif"));
             //tankD = ImageIO.read(ResourceManager.class.getClassLoader().getResourceAsStream("images/tankD.gif"));
-    
-            tankU = ImageIO.read(ResourceManager.class.getClassLoader().getResourceAsStream("images/BadTank1.png"));
-            tankL = ImageUtil.rotateImage(tankU, -90);
-            tankR = ImageUtil.rotateImage(tankU, 90);
-            tankD = ImageUtil.rotateImage(tankU, 180);
-    
-    
+            
+            goodTankU = ImageIO.read(ResourceManager.class.getClassLoader().getResourceAsStream("images/GoodTank1.png"));
+            goodTankL = ImageUtil.rotateImage(goodTankU, -90);
+            goodTankR = ImageUtil.rotateImage(goodTankU, 90);
+            goodTankD = ImageUtil.rotateImage(goodTankU, 180);
+            
+            badTankU = ImageIO.read(ResourceManager.class.getClassLoader().getResourceAsStream("images/BadTank1.png"));
+            badTankL = ImageUtil.rotateImage(badTankU, -90);
+            badTankR = ImageUtil.rotateImage(badTankU, 90);
+            badTankD = ImageUtil.rotateImage(badTankU, 180);
+            
+            
             //bulletL = ImageIO.read(ResourceManager.class.getClassLoader().getResourceAsStream("images/bulletL.gif"));
             //bulletU = ImageIO.read(ResourceManager.class.getClassLoader().getResourceAsStream("images/bulletU.gif"));
             //bulletR = ImageIO.read(ResourceManager.class.getClassLoader().getResourceAsStream("images/bulletR.gif"));
             //bulletD = ImageIO.read(ResourceManager.class.getClassLoader().getResourceAsStream("images/bulletD.gif"));
-    
+            
             bulletU = ImageIO.read(ResourceManager.class.getClassLoader().getResourceAsStream("images/bulletU.png"));
             bulletL = ImageUtil.rotateImage(bulletU, -90);
             bulletR = ImageUtil.rotateImage(bulletU, 90);
             bulletD = ImageUtil.rotateImage(bulletU, 180);
-    
+            
             //爆炸图全部加载到内存
             for (int i = 0; i < explodeArr.length; i++) {
                 explodeArr[i] = ImageIO.read(ResourceManager.class.getClassLoader().getResourceAsStream("images/" + "e" + (i + 1) + ".gif"));
