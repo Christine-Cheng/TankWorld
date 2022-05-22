0b51286caea4d1ba2ff9ee7c71b43323074b54e3
Happy <1737575350@qq.com>
2022-05-22 17:20:54 +0800

    添加背景音乐

--- src/com/tankwold/Main.java
+++ src/com/tankwold/Main.java
@@ -10,11 +10,15 @@ import com.tankwold.enums.Group;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         TankFrame frame = new TankFrame();
-        
+    
         //初始化敌方坦克
         for (int i = 0; i < 5; i++) {
             frame.enemyTankList.add(new Tank(50 + i * 80, 200, Direction.DOWN, Group.BAD, frame));
         }
+    
+        //初始化音乐
+        new Thread(() -> new Audio("audio/war1.wav").loop()).start();
+    
         while (true) {
             Thread.sleep(50);
             frame.repaint(); //You can use threads to refresh the window
